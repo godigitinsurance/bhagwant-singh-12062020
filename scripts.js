69fb8939af05fd6403c586c7ee1733625f714d17
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-06-12 08:22:20 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Bhagwant Singh");
+$("#policyNumber").text("EM06200605");
+$("#startDate").text("13-Jun-2020");
+$("#endDate").text("12-Jun-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB08DC1946");
+$("#make").text("HERO MOTOCORP");
+$("#model").text("PLEASURE");
+$("#vehicalSubType").text("ELECTRIC START DRUM BRAKE SPOKE WHEELS");
+$("#engineNo").text("JF16ECEGA00345");
+$("#chassis").text("MBLJF16EFEGA01730");
